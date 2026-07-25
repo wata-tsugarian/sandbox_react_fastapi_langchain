30822bd2b38ab2b3712392944d19cf97b23d1850
wata-tsugarian <wata.tsugarian@gmail.com>
2026-07-25 14:09:44 +0900

    エンドポイントのルーティングに変更

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from app.settings import settings
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def read_root():
+    return {
+        "env": settings.env,
+        "message": "Hello, World!",
+    }
+
+
+@app.get("/healthz")
+def health_check():
+    return {"status": "ok"}
