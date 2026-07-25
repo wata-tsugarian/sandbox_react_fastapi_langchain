a2c569e97a19a78898f8fd43e453284569f7e687
wata-tsugarian <wata.tsugarian@gmail.com>
2026-07-25 16:13:36 +0900

    Button Component配置

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function App() {
   return (
     <div className="flex justify-center items-center h-screen">
       <h1>トップページ</h1>
+      <Button>ボタン</Button>
     </div>
   );
 }
