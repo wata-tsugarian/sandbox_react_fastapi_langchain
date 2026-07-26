9f49bae28e92f3c30fdfa35f14a3fdbe3b81983b
wata-tsugarian <wata.tsugarian@gmail.com>
2026-07-26 11:38:21 +0900

    swrを利用したデータfetch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,45 @@
+import useSWRMutation from "swr/mutation";
 import { Button } from "@/components/ui/button";
 
+type FetchResult = {
+  env: string;
+  message: string;
+};
+
+async function fetchData(url: string): Promise<FetchResult> {
+  const response = await fetch(url, { method: "GET" });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error: ${response.status}`);
+  }
+
+  return await response.json();
+}
+
 export default function App() {
+  const { trigger, isMutating, data, error } = useSWRMutation(
+    "/api/",
+    fetchData,
+  );
+
   return (
     <div className="flex justify-center items-center h-screen">
-      <h1>トップページ</h1>
-      <Button>ボタン</Button>
+      <div className="flex flex-col w-[480px] gap-4">
+        <Button
+          className="cursor-pointer"
+          disabled={isMutating}
+          onClick={async () => {
+            await trigger();
+          }}
+        >
+          {isMutating ? "取得中..." : "fetch"}
+        </Button>
+        {error && <p className="text-red-500">エラー: {error.message}</p>}
+        <ul>
+          <li>env: {data?.env}</li>
+          <li>message: {data?.message}</li>
+        </ul>
+      </div>
     </div>
   );
 }
