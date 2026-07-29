95b43af44908c9dd8a85b347a353c3fdb793ace1
wata-tsugarian <wata.tsugarian@gmail.com>
2026-07-29 20:54:13 +0900

    読み出したドキュメントをチャンク化

---
+++ backend/app/ingestion/chunker.py
@@ -0,0 +1,20 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter, Language
+from langchain_core.documents import Document
+
+
+def create_chunks(raw_documents: list[Document]) -> list[Document]:
+    """ドキュメントをチャンク化
+
+    Args:
+        raw_documents: 読み出した生ドキュメント
+
+    Returns:
+        チャンクしたドキュメントのlist
+    """
+    text_splitter = RecursiveCharacterTextSplitter.from_language(
+        Language.MARKDOWN,
+        chunk_size=1000,
+        chunk_overlap=200,
+    )
+    documents = text_splitter.split_documents(raw_documents)
+    return documents
