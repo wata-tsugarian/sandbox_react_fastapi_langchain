ce2a4a9a58166b54340866b9c2918b7f93439acd
wata-tsugarian <wata.tsugarian@gmail.com>
2026-08-02 17:46:24 +0900

    メソッドに置換

--- backend/app/ingestion/invoker.py
+++ backend/app/ingestion/invoker.py
@@ -1,33 +1,15 @@
-import shutil
-from itertools import batched
-from pathlib import Path
-
-from langchain_chroma import Chroma
-from langchain_ollama import OllamaEmbeddings
-
 from app.ingestion.chunker import create_chunks
 from app.ingestion.loader import load_documents
-
-persist_directory = Path(__file__).parent.parent / "chroma_db"
+from app.middleware.vector_store import vector_store_client
 
 
 def main():
+    print("invoke開始")
     raw_documents = load_documents()
     chunks = create_chunks(raw_documents=raw_documents)
     print(f"{len(chunks)=}")
 
-    shutil.rmtree(persist_directory, ignore_errors=True)
-    embeddings = OllamaEmbeddings(model="bge-m3")
-    vector_store = Chroma(
-        embedding_function=embeddings,
-        persist_directory=str(persist_directory),
-    )
-
-    for i, chunk_group in enumerate(batched(chunks, 100)):
-        print(f"グループ_{i}: 処理中")
-        vector_store.add_documents(documents=chunk_group)
-
-    print(vector_store._collection.count())
+    vector_store_client.rebuild(chunks=chunks)
 
 
 if __name__ == "__main__":
